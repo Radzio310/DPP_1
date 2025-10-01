faaeaae8aea9a3407cf3cfd88456a4be3a5d7ce0
Radek Witkowicz <146724612+Radzio310@users.noreply.github.com>
2025-10-01 10:50:53 +0200

    Add Mandelbrot set drawing function [86c5ru202]
    Implement Mandelbrot set drawing in terminal.

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# file: main.py
+def draw_mandelbrot(w=80, h=24, it=25):
+    for y in range(h):
+        for x in range(w):
+            # rzutowanie pikseli terminala na płaszczyznę zespoloną
+            c = complex((x - w/2)/(w/4), (y - h/2)/(h/4))
+            z = 0
+            j = 0
+            while abs(z) <= 2 and j < it:
+                z = z*z + c
+                j += 1
+            print(" .:-=+*#%@"[j*10//it], end="")
+        print()
+
+def main():
+    draw_mandelbrot()  # możesz podać np. draw_mandelbrot(100, 30, 40)
+
+if __name__ == "__main__":
+    main()
